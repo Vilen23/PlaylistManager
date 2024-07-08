8905f5d17d4e59f9046214d6372e568520a58231
Vilen23 <shivamlather5@gmail.com>
2024-07-08 21:52:04 +0530

    dont know

--- app/newReleases/page.tsx
+++ app/newReleases/page.tsx
@@ -19,13 +19,15 @@ import { useRouter } from "next/navigation";
 export default function page() {
   const [loading, setLoading] = useRecoilState(loadingAtom);
   const [songs, setSongs] = useRecoilState(newReleaseAtom);
-  const [offset,setOffset] = useState(0);
+  const [offset, setOffset] = useState(0);
   const router = useRouter();
   useEffect(() => {
     const getrelease = async () => {
       try {
         setLoading(true);
-        const res = await axios.get("/api/getnewrelease?offset="+offset+"&limit=20");
+        const res = await axios.get(
+          "/api/getnewrelease?offset=" + offset + "&limit=20"
+        );
         console.log(res.data);
         setSongs(res.data.items);
         if (res.data.items.length > 0) {
@@ -45,32 +47,36 @@ export default function page() {
   return (
     <div>
       <LoveYourown />
-      <ExploreMusic/>
-     <div className="mb-10">
-     <Pagination>
-        <PaginationContent>
-          <PaginationItem>
-            <PaginationPrevious className="cursor-pointer" onClick={()=>{
-              if(offset==0){
-                setOffset(80);
-              }else setOffset(offset-20);
-            }}/>
-          </PaginationItem>
-          <PaginationEllipsis>
-
-          </PaginationEllipsis>
-          <PaginationItem>
-            <PaginationNext className="cursor-pointer" onClick={()=>{
-              if(offset==80){
-                setOffset(0);
-              }else{
-                setOffset(offset+20);
-              }
-            }}/>
-          </PaginationItem>
-        </PaginationContent>
-      </Pagination>
-     </div>
+      <ExploreMusic />
+      <div className="mb-10">
+        <Pagination>
+          <PaginationContent>
+            <PaginationItem>
+              <PaginationPrevious
+                className="cursor-pointer"
+                onClick={() => {
+                  if (offset == 0) {
+                    setOffset(80);
+                  } else setOffset(offset - 20);
+                }}
+              />
+            </PaginationItem>
+            <PaginationEllipsis></PaginationEllipsis>
+            <PaginationItem>
+              <PaginationNext
+                className="cursor-pointer"
+                onClick={() => {
+                  if (offset == 80) {
+                    setOffset(0);
+                  } else {
+                    setOffset(offset + 20);
+                  }
+                }}
+              />
+            </PaginationItem>
+          </PaginationContent>
+        </Pagination>
+      </div>
     </div>
   );
 }
